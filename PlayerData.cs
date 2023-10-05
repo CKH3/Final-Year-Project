8c3c99734bd8b2afd658f4891692fd084fc6524a
ffedmund <ffedmund@gmail.com>
2023-10-05 15:45:04 +0800

    Player Data

--- PlayerData.cs
+++ PlayerData.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerData{
-    readonly string[] attributeKeys = {"level","vitality","strength","intelligence","dexterity","endurance","luck"};
+    readonly string[] attributeKeys = {"level","honor","vitality","strength","intelligence","dexterity","endurance","luck"};
 
-    int _honor;
     Dictionary<string,int> _playerAttributesData;
 
     public PlayerData(){
-        _honor = 0;
         _playerAttributesData = new Dictionary<string, int>();
         foreach(string key in attributeKeys){
             _playerAttributesData[key] = 1;
         }
     }
 
-    
+    public void UpdatePlayerData(string key, int value, bool isReplace = false) {
+        if(_playerAttributesData.ContainsKey(key)){
+            int originalValue = _playerAttributesData[key];
+            _playerAttributesData[key] = isReplace? value:originalValue+value;
+        }
+    }
+
+    public int GetAttribute(string key){
+        return _playerAttributesData[key];
+    }
+
+    public Dictionary<string,int> GetAttributes(){
+        return _playerAttributesData;
+    }
 
 
 }
